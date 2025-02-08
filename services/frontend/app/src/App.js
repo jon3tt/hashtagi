4e4e3225ff9c328ccd56ec93a9b9ab40ce89c6db
jon3tt <jon3tt@outlook.com>
2025-02-09 01:29:12 +0200

    React-sovelluksen reititys ja navigointi Bootstrapin avulla
    - Käytetty React Routeria hallinnoimaan sovelluksen reititystä.
    - Lisätty Bootstrapin Navbar-komponentti navigointiin (Etusivu ja Blogi).
    - Reititetyt sivut:
      - Etusivu ("/")
      - Blogi ("/blog")
    - Navigointipalkki responsiivinen, ja käyttää Bootstrapin collapse-ominaisuutta pienillä näytöillä.
    -Lisätty bootstrap-icons paketti package.json

--- services/frontend/app/src/App.js
+++ services/frontend/app/src/App.js
@@ -1,26 +1,45 @@
+/* Järjestelmän komponentit, jotka mahdollistavat reitityksen eri sivujen välillä */
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+/* Sivujen komponentit, joita reititetään sovelluksessa */
 import Home from "./Home/Home";
 import Blog from "./Blog/Blog";
 
-import Container from "react-bootstrap/Container";
+/* Bootstrap-komponentit navigointia ja ulkoasua varten */
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-export default function MyApp() {
+/* Sovelluksen pääkomponentti, joka sisältää reitityksen ja navigointipalkin */
+export default function App() {
     return (
+        // Router-komponentti hallinnoi sovelluksen reititystä
         <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link element={Home} to={{pathname: "/"}}>Etusivu</Link>
-                    </li>
-                    <li>
-                        <Link element={Blog} to={{pathname: "/blog"}}>Blogi</Link>
-                    </li>
-                </ul>
-            </nav>
+            {/* Container navigointipalkille, joka käyttää koko näytön leveyttä */}
+            <Container fluid className="p-0 m-0" as="main">
+                {/* Bootstrapin Navbar-komponentti pääsivuston navigointiin */}
+                <Navbar expand="lg">
+                    {/* Sivuston brändin nimi */}
+                    <Navbar.Brand href="#">Hästägi</Navbar.Brand>
+
+                    {/* Toggler-kuvake pienten näyttöjen navigointia varten */}
+                    <Navbar.Toggle aria-controls="NavBar" />
+
+                    {/* Navigointilinkit (Etusivu ja Blogi) */}
+                    <Navbar.Collapse id="NavBar">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Etusivu</Nav.Link>
+                            <Nav.Link as={Link} to="/blog">Blogi</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container>
+
+            {/* Container, joka sisältää reititetyt sivut */}
             <Container as="main">
                 <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/blog" element={<Blog/>}/>
+                    {/* Etusivun reitti */}
+                    <Route path="/" element={<Home />} />
+                    {/* Blogi-sivun reitti */}
+                    <Route path="/blog" element={<Blog />} />
                 </Routes>
             </Container>
         </Router>
--- services/frontend/app/src/index.js
+++ services/frontend/app/src/index.js
@@ -1,9 +1,9 @@
 import {StrictMode} from "react";
 import ReactDOM from "react-dom/client";
-import MyApp from './App'
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <MyApp/>
+        <App/>
     </StrictMode>
 )
